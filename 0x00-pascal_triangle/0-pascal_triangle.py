062eba8d49808c036d22ea83b5a9b973701660a7
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-09-28 16:22:31 +0300

    Task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,11 +7,11 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     
-    triangle = []
-    for row in range(n):
-        triangle_row = [1] * (row + 1)
-        if row > 1:
-            for i in range(1, row):
-                triangle_row[i] = triangle[row - 1][i - 1] + triangle[row - 1][i]
+    triangle = [[1]]
+    for row in range(1, n):
+        triangle_row = [1]
+        for i in range(1, row):
+            triangle_row.append(triangle[row - 1][i - 1] + triangle[row - 1][i])
+        triangle_row.append(1)
         triangle.append(triangle_row)
     return triangle
