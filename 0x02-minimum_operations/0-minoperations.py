997fd0ade7f9a02adf252f9b87bf60939e051b04
Solomon Amsalu <solamsalu@gmail.com>
2023-10-12 16:51:15 +0300

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,36 +3,21 @@
 
 
 def minOperations(n):
-    """
-    Calculates the fewest number of operations needed to result in exactly n H characters in a file
-    Args:
-        n: an integer representing the target number of H characters
-    Returns:
-        an integer representing the minimum number of operations (copy all and paste)
-        or 0 if n is impossible to achieve
-    """
-    # If n is less than or equal to 0, return 0
+    """ Calculates the fewest number of operations needed to 
+    result in exactly n H characters in a file  """
     if n <= 0:
         return 0
-    # Initialize the number of operations to 0
     operations = 0
-    # Initialize the current number of H characters to 1
     current = 1
-    # Initialize a flag to indicate whether we have copied or not
     copied = False
-    # Loop until we reach n
     while current < n:
-        # If n is divisible by the current number, copy all and paste
         if n % current == 0:
-            operations += 2  # Copy all and paste are two operations
-            current *= 2  # Double the current number of H characters
-            copied = True  # Set the flag to true
-        # Otherwise, paste the copied number if we have copied before
+            operations += 2
+            current *= 2
+            copied = True  
         elif copied:
-            operations += 1  # Paste is one operation
-            current += current // 2  # Add half of the current number of H characters
-        # Otherwise, we cannot achieve n with the given operations, return 0
+            operations += 1  
+            current += current // 2  
         else:
             return 0
-    # Return the number of operations
     return operations
