a1478dee470a90185c814680613c855013344227
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-10-26 17:10:09 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,18 +7,29 @@ def validUTF8(data):
     continuation bytes expected """
     count = 0
     for num in data:
-        if count == 0:
-            if (num >> 5) == 0b110:
-                count = 1
-            elif (num >> 4) == 0b1110:
-                count = 2
-            elif (num >> 3) == 0b11110:
-                count = 3
-            elif (num >> 7):
-                return False
-        else:
+        # Ensure the number is within the valid range for a byte
+        if num < 0 or num >= 256:
+            return False
+
+        # If we're expecting continuation bytes, check if this byte is one
+        if count > 0:
             if (num >> 6) != 0b10:
                 return False
             count -= 1
-    return count == 0
-    
+            continue
+
+        # If this is a new character, determine its length based on the MSBs
+        if (num >> 5) == 0b110:
+            count = 1
+        elif (num >> 4) == 0b1110:
+            count = 2
+        elif (num >> 3) == 0b11110:
+            count = 3
+        elif (num >> 7) == 1:  # Single-byte characters must start with `0`
+            return False
+
+    # If we're still expecting continuation bytes, the data is invalid
+    if count > 0:
+        return False
+
+    return True
