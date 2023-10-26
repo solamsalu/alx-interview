afdae344063743b20e488221231f005d790f3903
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-10-26 16:43:13 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,24 +5,19 @@
 def validUTF8(data):
     """ Variable to keep track of the number of continuation bytes expected
     """
-    num_continuation_bytes = 0
-
-    for byte in data:
-        """ Check if it is a continuation byte """
-        if num_continuation_bytes > 0 and (byte >> 6) == 0b10:
-            num_continuation_bytes -= 1
-        elif num_continuation_bytes == 0:
-            if (byte >> 7) == 0b0:
-                num_continuation_bytes = 0
-            elif (byte >> 5) == 0b110:
-                num_continuation_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_continuation_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_continuation_bytes = 3
-            else:
+    count = 0
+    for num in data:
+        if count == 0:
+            if (num >> 5) == 0b110:
+                count = 1
+            elif (num >> 4) == 0b1110:
+                count = 2
+            elif (num >> 3) == 0b11110:
+                count = 3
+            elif (num >> 7):
                 return False
         else:
-            return False
-   
-    return True
+            if (num >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
