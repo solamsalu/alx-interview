80ff823f212afd03cb4c266465c83a6b06bb08d1
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-10-26 17:35:46 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,28 +9,25 @@ def validUTF8(data):
     """ Variable to keep track of the number of 
     continuation bytes expected """
 
-    count = 0
+    n_bytes = 0
+
     for num in data:
-        # Ensure the number is within the valid range for a byte
-        if not 0 <= num < 256:
-            return False
-
-        # If we're expecting continuation bytes, check if this byte is one
-        if count > 0:
-            if (num >> 6) != 0b10:
+        byte = num & 0xff
+        if n_bytes == 0:
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            while mask1 & byte:
+                n_bytes += 1
+                mask1 = mask1 >> 1
+                mask2 = mask2 >> 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
-            count -= 1
-            continue
-
-        # If this is a new character, determine its length based on the MSBs
-        if (num >> 5) == 0b110:
-            count = 1
-        elif (num >> 4) == 0b1110:
-            count = 2
-        elif (num >> 3) == 0b11110:
-            count = 3
-        elif (num >> 7) == 1:  # Single-byte characters must start with `0`
-            return False
-
-    # If we're still expecting continuation bytes, the data is invalid
-    return count == 0
+        else:
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
