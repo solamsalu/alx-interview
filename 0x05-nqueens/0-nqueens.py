8f9699f4186016461659a5d28884aa54675413eb
Solomon Amsalu <solamsalu@gmail.com>
2023-11-02 20:30:26 +0300

    Task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-"""N queens solution finder module.
-
-Usage: nqueens N:
-    If the user called the program with the wrong number of arguments, print
-        Usage: nqueens N, followed by a new line, and exit with the status 1.
-where N must be an integer greater or equal to 4
-    If N is not an integer, print N must be a number, followed by a new line,
-    and exit with the status 1.
-    If N is smaller than 4, print N must be at least 4, followed by a new
-    line, and exit with the status 1.
-The program should print every possible solution to the problem.
-    One solution per line.
-    Format: see README.
-    You don’t have to print the solutions in a specific order.
-You are only allowed to import the sys module.
-"""
+""" N queens solution finder module. """
 import sys
 
 
