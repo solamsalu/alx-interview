516da52e910ea1fc6520c87c9eeb387c88870432
Solomon Amsalu <solamsalu@gmail.com>
2023-11-09 17:30:56 +0300

    Task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+
+function getMovieCharacters(movieId) {
+    const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+    axios.get(movieUrl)
+        .then(response => {
+            const characters = response.data.characters;
+
+            characters.forEach(characterUrl => {
+                axios.get(characterUrl)
+                    .then(characterResponse => {
+                        const characterName = characterResponse.data.name;
+                        console.log(characterName);
+                    })
+                    .catch(error => {
+                        console.error(`Error fetching character data: ${error.message}`);
+                    });
+            });
+        })
+        .catch(error => {
+            console.error(`Error fetching movie data: ${error.message}`);
+        });
+}
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+    console.error('Usage: node script.js <Movie ID>');
+    process.exit(1);
+}
+
+getMovieCharacters(movieId);
